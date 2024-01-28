b72866b6a344aac0bb9492ed8ebc2de3eec9e884
coderbabu00 <coderbabu00@gmail.com>
2024-01-27 22:55:23 -0800

    Created login api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";  
 
@@ -27,4 +28,38 @@ export const signup = async (req, res, next) => {
     // or 
     next(err);
 }
+}
+
+export const signin = async (req, res, next) => {
+    try{
+    const {email, password}=req.body;
+
+    if (!email || !password || email === '' || password === '') {
+        next(errorHandler(400, 'All fields are required'));
+      }
+
+      const validUser = await User.findOne({ email });
+      if (!validUser) {
+        return next(errorHandler(404, 'User not found'));
+      }
+      const validPassword = bcrypt.compare(password, validUser.password);
+      if (!validPassword) {
+        return next(errorHandler(400, 'Invalid password'));
+      }
+      const token = jwt.sign(
+        { id: validUser._id, isAdmin: validUser.isAdmin },
+        process.env.JWT_SECRET
+      );
+  
+      const { password: pass, ...rest } = validUser._doc;
+  
+      res
+        .status(200)
+        .cookie('access_token', token, {
+          httpOnly: true,
+        })
+        .json(rest);
+    }catch(err){
+        next(err);
+    }
 }
